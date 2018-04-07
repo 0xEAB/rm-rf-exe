4460719c13549f2a30944eb4260d1a767719cc43
0xEAB <desisma@heidel.beer>
2018-04-07 22:01:00 +0200

    Implement support for multiple files

--- source/app.d
+++ source/app.d
@@ -3,11 +3,13 @@ import std.stdio : writeln;
 
 int main(string[] args)
 {
-    if (args.length != 2)
+    if (args.length < 2)
     {
         writeln("Usage:\trm-rf.exe dir");
         return 1;
     }
-    rmdirRecurseForce(args[1]);
+
+    foreach(string dir; args[1 .. $])
+        rmdirRecurseForce(dir);
     return 0;
 }
